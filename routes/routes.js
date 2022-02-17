1837f9bac32ab2ba1a4c8045a8677d2c274db55d
Juan Antonio Solares <solaresjuan43@gmail.com>
2022-02-16 21:54:49 -0600

    Fibonacci
    agregar funcionalidad fibonacci 201800496

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,23 @@ router.get('/api', (req, res) => {
 
 })
 
+// Fibonacci
+router.get('/fibo/:numero', (req = Request, res) => {
+
+    const { numero } = req.params
+
+    const nfibo = fib(numero);
+    return res.json({
+        fibonacci: nfibo
+    })
+})
+
+function fib(n) {
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
 
 module.exports = {
     router,
