447156fd61812360cea4783455c12583fe079231
gerson <gersonbelur@gmail.com>
2022-02-16 22:17:45 -0600

    Palindromo
    determinar si una palabra es palíndromo 
    201807228

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,27 @@ router.get('/api', (req, res) => {
 
 })
 
+router.get('/palindromo/:palabra',(req,res)=>{
+    var palabra = req.params.palabra
+    var pal = ""
+
+    for(var i = palabra.length-1; i >= 0; i--){
+        pal = pal + palabra[i]
+    }
+
+
+    if(palabra == pal){
+        res.json({
+            palindromo: true
+        })
+    }else{
+        res.json({
+            palindromo: false
+        })
+    }
+
+})
+
 
 module.exports = {
     router,
